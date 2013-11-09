5a18a722a3cc68e0d9cafdf9652da06e44f168e1
U-PS11-11\vantan <vantan@PS11-11.(none)>
2013-11-09 17:50:33 +0900

    shuffle

--- shuffle.c
+++ shuffle.c
@@ -5,34 +5,31 @@
 
 #define N 5
 
-void change(int a[],int from, int to)
-{
-	int temp = a[to];
-	a[to] = a[from];
-	a[from] = temp;
-}
-
 int main()
 {
-	srand((unsigned int)time(NULL));
+	srand(time(NULL));
 	
 	int a[N];
 	int i;
-	int roop;
+	int loop;
 	
-	while((roop = rand() % 20) == 0);
+	loop = rand() % 19 + 1;
 	
 	for( i=0; i<N; i++)
 	{
 		a[i] = i+1;
 	}
 	
-	for( i=0; i<roop; i++)
+	for( i=0; i<loop; i++)
 	{
-		change(a,rand() % N,rand() % N);
+		int from = rand() % N;
+		int to = rand() % N;
+		
+		int temp = a[to];
+		a[to] = a[from];
+		a[from] = temp;
 	}
 	
-	printf("roop = %d\n",roop);
 	for(i = 0; i<N; i++)
 	{
 		printf("[%d]",a[i]);
