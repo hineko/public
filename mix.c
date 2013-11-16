5424c3383f94c9f5a382a4f8422800c8e90bccdc
U-PS11-11\vantan <vantan@PS11-11.(none)>
2013-11-16 19:11:43 +0900

    mikanseigit add mix.c

--- mix.c
+++ mix.c
@@ -3,27 +3,60 @@
 
 #define BUFSIZ 1024
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char str1[BUFSIZ];
 	char str2[BUFSIZ];
+	char str3[BUFSIZ];
 
-	FILE *fpA, *fpB;
+	FILE *fp[argc-1];
+	
+	int i;
+	char c;
 
-	fpA = fopen("a.txt","r");
-	fpB = fopen("b.txt","r");
 	
-	assert(fpA);
-	assert(fpB);
-	
-	while(fgets(str1,BUFSIZ,fpA) && fgets(str2,BUFSIZ,fpB))
+	if(argc < 3)
 	{
-		printf("%s",str1);
-		printf("%s",str2);
+		fprintf(stderr,"usage: %s mix is no files...\n", argv[0]);
+		return 1;
 	}
 	
-	fclose(fpA);
-	fclose(fpB);
+	for(i=0;i<argc-1;i++)
+	{
+		printf("fp[%d] = [%s]\n",i,argv[i+1]);
+		fp[i] = fopen(argv[i+1],"r");
+		assert(fp[i] != NULL);
+	}
+
+	int j=0;
+	
+		for(i=0;i<argc-1;i++)
+		{
+			fgets(str1,BUFSIZ,fp[i]);
+			while(str1[j] != '\0')
+			{
+				j++;
+			}
+			
+			str1[j] = ' ';
+			printf("%s",str1);
+
+			
+			
+			
+			j=0;
+		}
+		printf("\0");
+	
+//	while(fgets(str1,BUFSIZ,fp[0]) && fgets(str2,BUFSIZ,fp[1]) && fgets(str3,BUFSIZ,fp[2]))
+//	{
+//		printf("%s %s %s\n",str1,str2,str3);
+//	}
+	
+	for(i=0;i<argc-1;i++)
+	{
+		fclose(fp[i]);
+	}
 	
 	return 0;
 }
