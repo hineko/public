39be53e2f85457984a49eea1c0d83f1b731bb458
U-PS11-11\vantan <vantan@PS11-11.(none)>
2013-12-14 18:51:10 +0900

    toktype1

---
+++ toktype.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+enum TokType
+{
+	OpPlus, OpMinus, OpEqual,
+	Number,EndOfFile,
+};
+
+
+static enum TokType
+gettok(char *buf, int bufsize)
+{
+	char c;
+	
+	while((c = getchar() != EOF)
+	{
+		if(isspace(c)) continue;
+		
+		switch(c)
+		{
+		case '+':
+			return OpPlus;
+			
+		case '-':
+			return OpMinus;
+			
+		case '=':
+			return OpEqual;
+			
+		default:
+			if(isdigit(c))
+			{
+				
+				
+				return Number;
+			}else{
+				fprintf(stderr,"invalid char '%c'\n",c);
+			}
+		}
+	}
+	
+	return EndOfFile;
+}
+
+
+int main()
+{
+	enum TokType tok;
+	char buf[100];
+
+	while((tok = gettok(buf,sizeof(buf))) != EndOfFile)
+	{
+		printf("tok = %d; buf = {%s}\n", tok, buf);
+	}
+	
+	return 0;
+}
