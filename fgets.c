25afe65cf6ad34c1022a87bf1fd0f20358d6f6d9
U-PS11-11\vantan <vantan@PS11-11.(none)>
2013-12-21 17:34:10 +0900

    c

--- fgets.c
+++ fgets.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
+#include <ctype.h>
 
 int main()
 {
-	char s[0];
+	char s[1];
 	if(fgets(s,sizeof(s),stdin) != NULL)
 	{
+		if(isspace(*s))
+		{
+			printf("a");
+		}
 		fputs(s,stdout);
-	} else 
+	} else
 	{
+		if(isspace(*s))
+		{
+			printf("b");
+		}		
 		fputs(s,stderr);
 	}
 	
