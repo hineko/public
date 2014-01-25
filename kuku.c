8c6f961ae6b68dc3ebf7d1dc6a4e3e722d07a56d
hineko <vantan@PS11-11.(none)>
2014-01-25 18:20:02 +0900

    adfka

--- kuku.c
+++ kuku.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
-#include <assert.h>
 
 int main(int args,char *argv[])
 {
 	int i,j;
-	FILE *fp;
 	
-	fp = fopen(argv[1],"w");
-	assert(fp != NULL);
-	
-	fprintf(fp, "<!DOCTYPE html>\n");
-	fprintf(fp, "<html>\n");
-	fprintf(fp, "<head><title>九九</title><meta charset=\"UTF-8\">\n");
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("<head><title>九九</title><meta charset=\"UTF-8\">\n");
 
-	fprintf(fp, "<body>\n");	
-	fprintf(fp, "\t<table border=\"1\">\n");
+	printf("<body>\n");	
+	printf("<table>\n");
 	
 	for(i=1;i<=9;i++)
 	{
-		fprintf(fp,"\t<tr>\n");
+		printf("<tr>\n");
 		for(j=1;j<=9;j++)
 		{
-			
-			fprintf(fp,"\t\t<td>%d * %d = %2d</td>\n",i,j,i*j);
-			
+			printf("<td> %2d </td>\n",i*j);
 		}
-		fprintf(fp,"\t</tr>\n");
+		printf("</tr>\n");
 	}
 	
-	fprintf(fp, "\t</table>\n");	
-	fprintf(fp, "</html>\n");
-	
-	
-	fclose(fp);
+	printf("</table>\n");	
+	printf("</html>\n");
 	
 	return 0;
 }
