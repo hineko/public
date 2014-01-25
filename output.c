9562a480ddfa8e4b1f53423231353075abc4019a
hineko <vantan@PS11-11.(none)>
2014-01-25 16:21:19 +0900

    a

--- output.c
+++ output.c
@@ -4,18 +4,30 @@
 // 吾輩はプログラムである。まだ中身はない
 int main(int argc, char* argv[])
 {
-	char c;
+	char c[100];
+	int i = 0;
 	
-	while((c=getchar()) != EOF)
+	
+	
+	while((fgets(c,100,stdin)) != NULL)
 	{
+loop:
 		printf("[");
 		
-		if( c != ' ')
+		while( c[i] != ' ')
 		{
-			printf("%c",c);
+			printf("%c",c[i]);
+			i++;
 		}
 		
 		printf("]\n");
+	
+		if( c[i] != '\n')
+		{
+			goto loop;
+		}
+		
+		i=0;
 	}
 	
 	
