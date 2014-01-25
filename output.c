4ab4b73a3171fba9a954a121dda722b2311972d5
hineko <vantan@PS11-11.(none)>
2014-01-25 16:28:36 +0900

    dkla

--- output.c
+++ output.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // 吾輩はプログラムである。まだ中身はない
 int main(int argc, char* argv[])
 {
 	char c[100];
-	int i = 0;
+	int i=0;
 	
-	
-	
-	while((fgets(c,100,stdin)) != NULL)
+	while(fgets(c,sizeof(c),stdin) != NULL)
 	{
 loop:
 		printf("[");
-		
+
 		while( c[i] != ' ')
 		{
 			printf("%c",c[i]);
 			i++;
 		}
-		
+		i++;
 		printf("]\n");
-	
 		if( c[i] != '\n')
 		{
 			goto loop;
@@ -29,8 +27,5 @@ loop:
 		
 		i=0;
 	}
-	
-	
-
 	return 0;
 }
