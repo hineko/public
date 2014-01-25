85c0b0fcc66c4fc6de652bbd0e4f9d068cfe6a79
hineko <vantan@PS11-11.(none)>
2014-01-25 14:29:08 +0900

    adc

--- output.c
+++ output.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // 吾輩はプログラムである。まだ中身はない
 int main(int argc, char* argv[])
 {
+	char tmp;
 	char c[100];
+	int i = 0;
 	
-	
-	while( c=getchar()) != EOF)
+	while( (tmp=getchar()) != EOF)
 	{
-		if(isspace(c))
+		if(isspace(tmp))
 		{
-			
+			// スペースは単語の区切り
+			if(i<=0)
+			{
+				continue;
+			}
+			else
+			{
+				printf("[%s]\n",c);
+				memset(c,'\0',100);
+				i=0;
+				continue;
+			}
+		}
+		else
+		{
+			c[i] = tmp;
+			i++;
 		}
 	
 	}
